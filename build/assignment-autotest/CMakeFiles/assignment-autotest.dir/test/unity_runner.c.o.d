28b19037ed1b2544497de19672fb36324be02860
BilalAzhar0 <96930930+BilalAzhar0@users.noreply.github.com>
2022-11-18 08:29:24 +0500

    Add files via upload

---
+++ build/assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o.d
@@ -0,0 +1,3 @@
+assignment-autotest/CMakeFiles/assignment-autotest.dir/test/unity_runner.c.o: \
+ /home/bilal/Documents/linux-course/assignment-1-BilalAzhar0/assignment-autotest/test/unity_runner.c \
+ /usr/include/stdc-predef.h
